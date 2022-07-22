d4f2598bf4fe1ded874b5cd0347470458b694b17
Adrian Barbos <adrianb@ensemble.com>
2022-07-22 11:32:35 +0300

    feature/FM-3 Implemented login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AuthTemplate } from "../../common/templates/AuthTemplate";
+import "./Login.css";
+import { MailOutlined, UnlockOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // const onLogin2 = async () => {
+  //   const reponse = await axios.post("http://localhost:3000/auth/login", {
+  //     email: email,
+  //     password: password,
+  //   });
+
+  //   console.log(reponse.data);
+  // };
+
+  const onLogin = () => {
+    axios.post("http://localhost:3000/auth/login", {
+      email: email,
+      password: password,
+    }).then(response => {
+      console.log("Success")
+      console.log(response.data);
+    }).catch(e => {
+      console.log("Error")
+      console.log(e)
+    });
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <AuthTemplate>
+      <h1 className="login-title">Login</h1>
+      <div className="login-field">
+        <MailOutlined style={{ color: "#A59591" }} />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="login-field">
+        <UnlockOutlined style={{ color: "#A59591" }} />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="login-button-container">
+        <button className="login-button" onClick={onLogin}>
+          Log In
+        </button>
+      </div>
+    </AuthTemplate>
+  );
+};
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import "./Welcome.css";
+import { Link } from "react-router-dom";
+import { AuthTemplate } from "../../common/templates/AuthTemplate";
 
 export const Welcome = () => {
   return (
-    <div className="auth-container">
-      <div className="auth-form-container">
-        <div className="auth-form">
-          <h1 className="auth-form__title">FOOD MOOD</h1>
-          <h3 className="auth-form__subtitle">DIFFERENT DELICIOUS SIMPLE</h3>
-          <p className="auth-form__description">
-            Trying to find your taste? <br/> Sign Up and let’s go on a delicious
-            journey!
-          </p>
-          <div className="auth-form__buttons-container">
-            <a href="/register">Sign Up</a>
-            <a href="/login">Log In</a>
-          </div>
-        </div>
+    <AuthTemplate>
+      <h1 className="welcome__title">FOOD MOOD</h1>
+      <h3 className="welcome__subtitle">DIFFERENT DELICIOUS SIMPLE</h3>
+      <p className="welcome__description">
+        Trying to find your taste? <br /> Sign Up and let’s go on a delicious
+        journey!
+      </p>
+      <div className="welcome__buttons-container">
+        <Link to="/register">Sign Up</Link>
+        <Link to="/login">Log In</Link>
       </div>
-    </div>
+    </AuthTemplate>
   );
 };
