112b2b6883546f0f8449d53b81c3bde436d74fe9
Andoni Laurentiu <iliuta.andoni01@e-uvt.ro>
2022-07-25 15:57:04 +0300

    Login refactoring & CSS fixing

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
 import { AuthTemplate } from "../../common/templates/AuthTemplate";
 import "./Login.css";
-import { MailOutlined, UnlockOutlined, LeftOutlined } from "@ant-design/icons";
+import { MailOutlined, UnlockOutlined, LeftOutlined, EyeOutlined } from "@ant-design/icons";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  // const onLogin2 = async () => {
-  //   const reponse = await axios.post("http://localhost:3000/auth/login", {
-  //     email: email,
-  //     password: password,
-  //   });
-
-  //   console.log(reponse.data);
-  // };
 
   const onLogin = () => {
     axios
@@ -36,40 +27,48 @@ export const Login = () => {
 
   return (
     <AuthTemplate>
-      <header>
+      <header className="header">
         <LeftOutlined style={{ color: "#5F7D63" }} />
-        <h1 className="login-title">Login</h1>
+        <h1 className="header__title">Login</h1>
       </header>
-      <div className="login-field">
-        <MailOutlined style={{ color: "#A59591" }} />
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="login-field">
-        <UnlockOutlined style={{ color: "#A59591" }} />
+
+
+
+      <main className="main">
+        <div className="email">
+          <MailOutlined className="email-icon" style={{ color: "#A59591" }} />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+      <div className="password">
+        <UnlockOutlined style={{ color: "#A59591"}} />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <EyeOutlined className="password-icon" style={{ color: "#A59591"}}/>
       </div>
       <div className="login-options-container">
         <div className="logged-container">
-          <input type="checkbox" />
+          <input type="checkbox" className="logged-checkbox"/>
           <p>Keep me signed in</p>
         </div>
         <Link to="/recovery">Trouble logging in?</Link>
       </div>
+      <button className="login-button" onClick={onLogin}>
+        Log In
+      </button>
+      </main>
+
+
       <footer className="login-page-footer">
         <div className="login-button-container">
-          <button className="login-button" onClick={onLogin}>
-            Log In
-          </button>
           <p>
             You don't have an account yet? {<Link to="/register">Sign Up</Link>}
           </p>
