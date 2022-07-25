e04e479a17cb8999643e64cd664a660b47a4f8b9
Gheorghe Alex <galex.marius@gmail.com>
2022-07-25 11:20:54 +0300

    feature/FM-3 Added UI elements and basic styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { AuthTemplate } from "../../common/templates/AuthTemplate";
 import "./Login.css";
-import { MailOutlined, UnlockOutlined } from "@ant-design/icons";
+import { MailOutlined, UnlockOutlined, LeftOutlined } from "@ant-design/icons";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // const onLogin2 = async () => {
   //   const reponse = await axios.post("http://localhost:3000/auth/login", {
@@ -18,21 +19,27 @@ export const Login = () => {
   // };
 
   const onLogin = () => {
-    axios.post("http://localhost:3000/auth/login", {
-      email: email,
-      password: password,
-    }).then(response => {
-      console.log("Success")
-      console.log(response.data);
-    }).catch(e => {
-      console.log("Error")
-      console.log(e)
-    });
+    axios
+      .post("http://localhost:3000/auth/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log("Success");
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log("Error");
+        console.log(e);
+      });
   };
 
   return (
     <AuthTemplate>
-      <h1 className="login-title">Login</h1>
+      <header>
+        <LeftOutlined style={{ color: "#5F7D63" }} />
+        <h1 className="login-title">Login</h1>
+      </header>
       <div className="login-field">
         <MailOutlined style={{ color: "#A59591" }} />
         <input
@@ -51,11 +58,23 @@ export const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <div className="login-button-container">
-        <button className="login-button" onClick={onLogin}>
-          Log In
-        </button>
+      <div className="login-options-container">
+        <div className="logged-container">
+          <input type="checkbox" />
+          <p>Keep me signed in</p>
+        </div>
+        <Link to="/recovery">Trouble logging in?</Link>
       </div>
+      <footer className="login-page-footer">
+        <div className="login-button-container">
+          <button className="login-button" onClick={onLogin}>
+            Log In
+          </button>
+          <p>
+            You don't have an account yet? {<Link to="/register">Sign Up</Link>}
+          </p>
+        </div>
+      </footer>
     </AuthTemplate>
   );
 };
