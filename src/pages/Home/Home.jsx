fd63d310fda5262fbdcd3cf95e1b384fb929c16f
‘Catalin <sofroni.catalin@gmail.com>
2022-08-04 09:28:25 +0300

    feature/FM-5 Partial Commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,139 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Home.less";
+import { Button, Col, Input, Layout, Menu, Row } from "antd";
+import {
+  ShoppingCartOutlined,
+  SettingOutlined,
+  UserOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
+import RestaurantsList from "./RestaurantsList";
+
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
+
+const items = [
+  {
+    key: "cart",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    key: "SubMenu",
+    icon: <UserOutlined />,
+    children: [
+      {
+        label: "Account",
+        icon: <UserOutlined />,
+      },
+      {
+        label: "Preferences",
+        icon: <SettingOutlined />,
+      },
+      {
+        label: "Log out",
+        icon: <LogoutOutlined />,
+      },
+    ],
+  },
+];
+
+const onSearch = (value) => console.log(value);
 
 const Home = () => {
-  return (
-    <div>
-      RESTAURANTS
-    </div>
-  )
-}
+  const [current, setCurrent] = useState("mail");
 
-export default Home
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <div className="home-header">
+          <div className="logo">FOOD MOOD</div>
+          <div>
+            <Search
+              placeholder="Search"
+              onSearch={onSearch}
+              enterButton
+              allowClear
+            />
+          </div>
+          <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+          />
+        </div>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{
+          padding: "0 50px",
+          marginTop: 64,
+        }}
+      >
+        <div
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            minHeight: 380,
+          }}
+        >
+          <div>
+            <Row gutter={[16, 16]}>
+              <Col span={24}>
+                <div className="home-title">Restaurants</div>
+              </Col>
+              <Col span={24}>
+                <div className="home-subtitle">
+                  <div>Can’t decide ?</div>
+                  <div>Not what you’re looking for? Let us help you!</div>
+                  <Button
+                    type="primary"
+                    style={{
+                      fontFamily: "Inter",
+                      fontStyle: "normal",
+                      fontWeight: "700",
+                      width: "7.5rem",
+                    }}
+                  >
+                    Mood me up
+                  </Button>
+                </div>
+              </Col>
+
+              {RestaurantsList.map(({ name, preferences }, index) => {
+                return (
+                  <Col className="gutter-row" span={6}>
+                    <div className="img_res">{name}</div>
+                    <div>{preferences}</div>
+                  </Col>
+                );
+              })}
+              
+            </Row>
+          </div>
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        ©2022 Created by ...
+      </Footer>
+    </Layout>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/RestaurantsList.jsx
@@ -0,0 +1,17 @@
+
+const RestaurantsList = [
+  { id: 1, name: "Restaurant 1", preferences: ["Healthy", "Greasy"] },
+  { id: 2, name: "Restaurant 2", preferences: ["Healthy", "Cheesy"] },
+  { id: 3, name: "Restaurant 3", preferences: ["Healthy", "Vegetarian"] },
+  { id: 4, name: "Restaurant 4", preferences: ["Greasy", "Cheesy"] },
+  { id: 5, name: "Restaurant 5", preferences: ["Greasy", "Vegetarian"] },
+  { id: 6, name: "Restaurant 6", preferences: ["Vegetarian", "Vegan"] },
+  { id: 7, name: "Restaurant 7", preferences: ["Vegan", "Meat lover"] },
+  { id: 8, name: "Restaurant 8", preferences: ["Meat lover", "Gourmand"] },
+  { id: 9, name: "Restaurant 9", preferences: ["Meat lover", "Gourmet"] },
+  { id: 10, name: "Restaurant 10", preferences: ["Gourmand", "Gourmet"] },
+];
+
+  //preferencesList = ["Healthy", "Greasy", "Cheesy", "Vegetarian", "Vegan", "Meat lover", "Gourmand", "Gourmet", "Gluten free"];
+
+export default RestaurantsList;
