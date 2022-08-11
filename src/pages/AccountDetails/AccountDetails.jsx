68419837e298df0e9343e845eed1181ada1ebdbf
GheorgheAlex <galex.marius@gmail.com>
2022-08-11 09:13:03 +0300

    feature/FM-9 Update profile now sends data to backend; validations are working and alerts shows on screen. Added html title and icon

--- src/pages/AccountDetails/AccountDetails.jsx
+++ src/pages/AccountDetails/AccountDetails.jsx
@@ -2,27 +2,57 @@ import { useEffect, useState } from "react";
 import "./AccountDetails.css";
 import MainPageTemplate from "../../common/templates/MainPageTemplate.jsx";
 import { Content, Header } from "antd/lib/layout/layout";
-import { Input, Typography, Row, Col, Form, Select, Button } from "antd";
+import {
+  Input,
+  Typography,
+  Row,
+  Col,
+  Form,
+  Select,
+  Button,
+  message,
+} from "antd";
 import { LeftOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { judete } from "../../common/data/judete.json";
+import axios from "axios";
 
 const { Title } = Typography;
 
 const AccountDetails = () => {
   // Form states
-  const [userName, setUserName] = useState("Alex");
+  const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [address, setAddress] = useState("");
   const [selectedCounty, setSelectedCounty] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
 
+  // const [currentPassword, setCurrentPassword] = useState("");
+  // const [newPassword, setNewPassword] = useState("");
+  // const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  // const [changePassword, setChangePassword] = useState(false);
+
   const [availableCounties, setAvailableCounties] = useState([]);
   const [availableCities, setAvailableCities] = useState([]);
 
+  const [errors, setErrors] = useState({});
+
   const onUserNameEdit = (e) => setUserName(e);
-  //TODO: Make functions to set Form Field states
+  const onEmailEdit = (e) => setEmail(e);
+  const onPhoneNumberEdit = (e) => setPhoneNumber(e);
+  const onAddressEdit = (e) => setAddress(e);
+
+  // const onCurrentPasswordEdit = (e) => setCurrentPassword(e);
+  // const onNewPasswordEdit = (e) => setNewPassword(e);
+  // const onConfirmNewPasswordEdit = (e) => {
+  //   setConfirmNewPassword(e);
+  //   if (confirmNewPassword.trim() === "") {
+  //     setChangePassword(false);
+  //   } else {
+  //     setChangePassword(true);
+  //   }
+  // };
 
   const onCountySelect = (e) => {
     setSelectedCounty(e);
@@ -39,10 +69,96 @@ const AccountDetails = () => {
     setSelectedCity(e);
   };
 
-  //TODO: Submit button on click function to send data to backend
+  const success = (messageContent) => {
+    message.success({
+      content: messageContent,
+      className: "success-message",
+      style: {
+        marginTop: "7.5vh",
+      },
+      icon: " ",
+    });
+  };
+
+  const failed = () => {
+    message.error({
+      content: "Couldn't update your profile",
+      className: "error-message",
+      style: {
+        marginTop: "7.5vh",
+      },
+      icon: " ",
+    });
+  };
+
+  //TODO: Validations
+
+  const onProfileUpdate = async () => {
+    const token = JSON.parse(sessionStorage.getItem("user")).access_token;
+    await axios
+      .put(
+        `${
+          import.meta.env.VITE_API_URL ||
+          "http://localhost:3000/users/profileUpdate"
+        }`,
+        {
+          name: userName,
+          // email: email,
+          phoneNumber: phoneNumber,
+          address: address,
+          county: selectedCounty,
+          city: selectedCity,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        setErrors({});
+        success("Successfully updated your information.");
+      })
+      .catch((error) => {
+        if (error.response) {
+          setErrors(error.response.data.message);
+        }
+        failed();
+      });
+  };
 
   useEffect(() => {
-    // TODO: Take information stored in database
+    // TODO: Take information from database
+    const token = JSON.parse(sessionStorage.getItem("user")).access_token;
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_API_URL ||
+          "http://localhost:3000/users/profileData"
+        }`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setUserName(response.data.userData.name);
+        setEmail(response.data.userData.email);
+        setPhoneNumber(response.data.userData.phoneNumber);
+        setAddress(response.data.userData.address);
+        setSelectedCounty(response.data.userData.county);
+        setSelectedCity(response.data.userData.city);
+
+        const cities = judete.filter(
+          (judet) => judet.nume === response.data.userData.county
+        )[0].localitati;
+        const citiesAscending = [...cities].sort((a, b) =>
+          a.nume > b.nume ? 1 : -1
+        );
+        setAvailableCities(citiesAscending);
+      })
+      .catch((e) => {
+        // e.response.data.message);
+        console.log(e.response);
+      });
     setAvailableCounties(judete);
   }, []);
 
@@ -82,28 +198,76 @@ const AccountDetails = () => {
                   size: "default",
                 }}
               >
-                <Form.Item label="Name">
+                {/*TODO: Form validations venite din backend pentru campuri*/}
+                <Form.Item
+                  label="Name"
+                  validateStatus={
+                    errors.hasOwnProperty("name") ? "error" : "validating"
+                  }
+                  help={errors.hasOwnProperty("name") ? errors.name : null}
+                >
                   <Input
                     className="form-input"
-                    defaultValue={userName}
+                    value={userName}
                     onChange={(e) => onUserNameEdit(e.target.value)}
                   />
                 </Form.Item>
 
-                <Form.Item label="Email">
-                  <Input className="form-input" />
+                <Form.Item
+                  label="Email"
+                  validateStatus={
+                    errors.hasOwnProperty("email") ? "error" : "validating"
+                  }
+                  help={errors.hasOwnProperty("email") ? errors.email : null}
+                >
+                  <Input
+                    className="form-input"
+                    value={email}
+                    onChange={(e) => onEmailEdit(e.target.value)}
+                  />
                 </Form.Item>
 
-                <Form.Item label="Phone number">
-                  <Input className="form-input" />
+                <Form.Item
+                  label="Phone number"
+                  validateStatus={
+                    errors.hasOwnProperty("phoneNumber")
+                      ? "error"
+                      : "validating"
+                  }
+                  help={
+                    errors.hasOwnProperty("phoneNumber")
+                      ? errors.phoneNumber
+                      : null
+                  }
+                >
+                  <Input
+                    className="form-input"
+                    value={phoneNumber}
+                    onChange={(e) => onPhoneNumberEdit(e.target.value)}
+                  />
                 </Form.Item>
 
-                <Form.Item label="Address">
-                  <Input className="form-input" />
+                <Form.Item
+                  label="Address"
+                  validateStatus={
+                    errors.hasOwnProperty("address") ? "error" : "validating"
+                  }
+                  help={
+                    errors.hasOwnProperty("address") ? errors.address : null
+                  }
+                >
+                  <Input
+                    className="form-input"
+                    value={address}
+                    onChange={(e) => onAddressEdit(e.target.value)}
+                  />
                 </Form.Item>
 
                 <Form.Item label="County">
-                  <Select onChange={(e) => onCountySelect(e)}>
+                  <Select
+                    value={selectedCounty}
+                    onChange={(e) => onCountySelect(e)}
+                  >
                     {availableCounties.map((county, key) => {
                       return (
                         <Select.Option value={county.nume} key={key}>
@@ -148,15 +312,22 @@ const AccountDetails = () => {
                   label="Current password"
                   className="password-field-label"
                 >
-                  <Input.Password className="form-input" />
+                  <Input.Password
+                    className="form-input"
+                    // onChange={(e) => onCurrentPasswordEdit(e.target.value)}
+                  />
                 </Form.Item>
 
                 <Form.Item label="New password">
-                  <Input.Password />
+                  <Input.Password
+                  // onChange={(e) => onNewPasswordEdit(e.target.value)}
+                  />
                 </Form.Item>
 
                 <Form.Item label="Confirm new password">
-                  <Input.Password />
+                  <Input.Password
+                  // onChange={(e) => onConfirmNewPasswordEdit(e.target.value)}
+                  />
                 </Form.Item>
 
                 <Form.Item>
@@ -164,14 +335,15 @@ const AccountDetails = () => {
                     type="primary"
                     htmlType="submit"
                     className="submitButton"
+                    onClick={onProfileUpdate}
                   >
                     Save Changes
                   </Button>
                 </Form.Item>
               </Form>
 
+              {/*//TODO: Move "Save Changes" button into correct position*/}
               {/*  <Col span={6}></Col>*/}
-
               {/*  <Col span={6} className="submit-button-container">*/}
               {/*    <Button*/}
               {/*      type="primary"*/}
